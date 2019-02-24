dfb8137ec6b513f315d61ddc04c33c5ae5e5f348
Mac Tan <met359@nyu.edu>
2019-02-24 02:48:36 -0500

    Began scraping 2001 census data

--- Code/Data processing/process_demographic_data.R
+++ Code/Data processing/process_demographic_data.R
@@ -19,21 +19,19 @@ demographics <- fread("Data/Raw/Demographics/districts_sex_age_education_2016.cs
   as.data.frame() %>%
   as.tbl() %>%
   filter(district_code > 10000, age %in% c("15 to 24 years", "25 to 29 years", "30 to 34 years", "35 to 44 years", "45 to 54 years",
-                                           "55 to 64 years", "65 to 74 years", "75 years and over", "Total")) %>%
+                                           "55 to 64 years", "65 to 74 years", "75 years and over"),
+         sex != "Total", education != "Total") %>%
   mutate(education = case_when(education %in% c("No certificate, diploma or degree",
                                                 "Secondary (high) school diploma or equivalency certificate") ~ "HS or less",
                                education %in% c("Apprenticeship or trades certificate or diploma",
                                                 "College, CEGEP or other non-university certificate or diploma",
                                                 "University certificate or diploma below bachelor level") ~ "College / some university",
-                               education %in% c("University certificate, diploma or degree at bachelor level or above") ~ "Bachelor's or above",
-                               education %in% c("Bachelor's degree") ~ "Bachelor's only",
-                               education %in% c("University certificate, diploma or degree above bachelor level") ~ "Graduate degree",
-                               education == "Total" ~ "Total"),
+                               education %in% c("University certificate, diploma or degree at bachelor level or above") ~ "Bachelor's or above"),
          age = case_when(age %in% c("15 to 24 years", "25 to 29 years") ~ "15 to 29 years",
                          age %in% c("30 to 34 years", "35 to 44 years") ~ "30 to 44 years",
                          age %in% c("45 to 54 years", "55 to 64 years") ~ "45 to 64 years",
-                         age %in% c("65 to 74 years", "75 years and over") ~ "65 and older",
-                         age == "Total" ~ "Total")) %>%
+                         age %in% c("65 to 74 years", "75 years and over") ~ "65 and older")) %>%
+  na.omit() %>%
   group_by(year, district_code, district_name, age, sex, education) %>%
   summarise(total_people = sum(total_people)) %>%
   ungroup()
