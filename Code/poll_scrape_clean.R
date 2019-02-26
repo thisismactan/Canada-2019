270c2024007c365b826d78666529c97a3c331d8c
Mac Tan <met359@nyu.edu>
2019-02-26 11:38:52 -0500

    Robustified scraper

--- Code/poll_scrape_clean.R
+++ Code/poll_scrape_clean.R
@@ -21,9 +21,8 @@ national_polls <- rvest::html_nodes(polls_GET, xpath = header_string)[1] %>%
   filter(Polling.firm != Last.dateof.polling, Polling.firm != "2015 Election") %>%
   
   # Clean up
-  dplyr::select(pollster = Polling.firm, last_date = Last.dateof.polling, MOE = Marginof.error.1., n = Samplesize.2.,
-                mode = Polling.method.3., LPC, CPC, NDP, BQ, GPC, PPC) %>%
-  replace_na(list(pollster = "", last_date = NA, MOE = "0 pp", mode = "", LPC = NA, CPC = NA, NDP = NA, BQ = NA, GPC = NA, PPC = NA)) %>%
+  dplyr::select(pollster = 1, last_date = 2, MOE = 10, n = 11, mode = 12, LPC, CPC, NDP, BQ, GPC, PPC = 9) %>%
+  replace_na(list(pollster = "", last_date = NA, MOE = "0 pp", n = "", mode = "", LPC = NA, CPC = NA, NDP = NA, BQ = NA, GPC = NA, PPC = NA)) %>%
   mutate(last_date = as.Date(last_date, format = "%B %d, %Y"),
          n = str_split(n, " \\(") %>% sapply(head, 1),
          n = gsub(",", "", n),
@@ -32,13 +31,15 @@ national_polls <- rvest::html_nodes(polls_GET, xpath = header_string)[1] %>%
          IVR = grepl("IVR", mode),
          online = grepl("online", mode)) %>%
   mutate_at(vars(c("MOE", "n", "LPC", "CPC", "NDP", "BQ", "GPC", "PPC")), as.numeric) %>%
+  mutate(PPC = replace(PPC, which(PPC == 0), NA)) %>%
+  filter(!is.na(LPC)) %>%
   
   # Estimate MOE from n for polls which have no MOE
   mutate(MOE = case_when(MOE != 0 ~ MOE,
                          (MOE == 0 | is.na(MOE)) ~ 1.3*sqrt(2500/n))) %>%
   
   # Merge in date spreads by pollster
-  merge(read.csv("Data/poll_spreads.csv", stringsAsFactors = FALSE), by = "pollster", all.x = TRUE) %>%
+  merge(read_csv("Data/poll_spreads.csv"), by = "pollster", all.x = TRUE) %>%
   
   # Calculate poll age
   mutate(date = last_date - floor(spread/2),
