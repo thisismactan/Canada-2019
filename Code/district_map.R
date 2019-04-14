bdba6b7d9c842a4a95abebca2a2ebb1ab1d998c0
Mac Tan <mactan@gmail.com>
2019-04-14 16:25:52 -0400

    Attempting to add CSS to app

--- Code/district_map.R
+++ Code/district_map.R
@@ -17,35 +17,40 @@ canada_districts_latlong <- spTransform(canada_districts, CRS("+proj=longlat +da
   merge(NDP_distribution %>% dplyr::select(-name_english), by.x = "FED_NUM", by.y = "district_code", all.x = TRUE) %>%
   merge(Bloc_distribution %>% dplyr::select(-name_english), by.x = "FED_NUM", by.y = "district_code", all.x = TRUE) %>%
   merge(Green_distribution %>% dplyr::select(-name_english), by.x = "FED_NUM", by.y = "district_code", all.x = TRUE) %>%
+  merge(PPC_distribution %>% dplyr::select(-name_english), by.x = "FED_NUM", by.y = "district_code", all.x = TRUE) %>%
   merge(district_probs %>% dplyr::select(-name_english), by.x = "FED_NUM", by.y = "district_code", all.x = TRUE) %>%
   merge(read_csv("Data/incumbents.csv") %>% dplyr::select(-name_english), by.x = "FED_NUM", by.y = "district_code", all.x = TRUE) %>%
   merge(cands_2019, by.x = "FED_NUM", by.y = "district_code") %>%
   as.data.frame() %>%
-  mutate(max_prob = pmax(LPC_prob, CPC_prob, NDP_prob, Bloc_prob, Green_prob),
+  mutate(max_prob = pmax(LPC_prob, CPC_prob, NDP_prob, Bloc_prob, Green_prob, PPC_prob),
          predicted_winner = case_when(LPC_prob == max_prob ~ "Liberal",
                                       CPC_prob == max_prob ~ "Conservative",
                                       NDP_prob == max_prob ~ "NDP",
                                       Bloc_prob == max_prob ~ "Bloc",
-                                      Green_prob == max_prob ~ "Green"),
+                                      Green_prob == max_prob ~ "Green",
+                                      PPC_prob == max_prob ~ "People's Party"),
          last_color = case_when(last_winner == "Liberal" ~ "'red'",
                                 last_winner == "Conservative" ~ "'blue'",
                                 last_winner == "NDP" ~ "'#EE7600'",
                                 last_winner == "Bloc" ~ "'#8B008B'",
-                                last_winner == "Green" ~ "'#008B00'"),
+                                last_winner == "Green" ~ "'#008B00'",
+                                last_winner == "People's Party" ~ "'#191970'"),
          predicted_color = case_when(predicted_winner == "Liberal" ~ "'red'",
                                      predicted_winner == "Conservative" ~ "'blue'",
                                      predicted_winner == "NDP" ~ "'#EE7600'",
                                      predicted_winner == "Bloc" ~ "'#8B008B'",
-                                     predicted_winner == "Green" ~ "'#008B00'"),
+                                     predicted_winner == "Green" ~ "'#008B00'",
+                                     predicted_winner == "People's Party" ~ "'#191970'"),
          hold = (predicted_winner == last_winner),
          fill_color = case_when(LPC_prob == max_prob ~ "red",
                                 CPC_prob == max_prob ~ "blue",
                                 NDP_prob == max_prob ~ "#EE7600",
                                 Bloc_prob == max_prob ~ "#8B008B",
-                                Green_prob == max_prob ~ "#008B00"),
+                                Green_prob == max_prob ~ "#008B00",
+                                PPC_prob == max_prob ~ "#191970"),
          district_info = case_when(
            ## Holds in Quebec
-           PROVCODE == "QC" & predicted_winner == last_winner ~
+           PROVCODE == "QC" & name_english != "Beauce" & predicted_winner == last_winner ~
              paste0("<b><u><font size = 2.5>", name_english, "</font></b></u><br>",
                     "<b><font color = ", predicted_color, ">", predicted_winner, "</font> hold</b><br>",
                     "<br>",
@@ -64,7 +69,7 @@ canada_districts_latlong <- spTransform(canada_districts, CRS("+proj=longlat +da
                     "<font color = '#008B00'><b>Green</b></font>: ", round(100*pct_50.Green, 1), "% (", round(100*pct_05.Green, 1), "%–", round(100*pct_95.Green, 1), "%)"),
            
            ## Flips in Quebec
-           PROVCODE == "QC" & predicted_winner != last_winner ~
+           PROVCODE == "QC" & name_english != "Beauce" & predicted_winner != last_winner ~
              paste0("<b><u><font size = 2.5>", name_english, "</font></b></u><br>",
                     "<b><font color = ", predicted_color, ">", predicted_winner, "</font> gain from ", "<font color = ", last_color, ">", last_winner, "</font></b><br>",
                     "<br>",
@@ -82,6 +87,49 @@ canada_districts_latlong <- spTransform(canada_districts, CRS("+proj=longlat +da
                     "<font color = '#8B008B'><b>Bloc</b></font>: ", round(100*pct_50.Bloc, 1), "% (", round(100*pct_05.Bloc, 1), "%–", round(100*pct_95.Bloc, 1), "%)<br>",
                     "<font color = '#008B00'><b>Green</b></font>: ", round(100*pct_50.Green, 1), "% (", round(100*pct_05.Green, 1), "%–", round(100*pct_95.Green, 1), "%)"
                     ),
+           
+           ## Beauce (Maxime Bernier)
+           name_english == "Beauce" & predicted_winner == last_winner ~
+             paste0("<b><u><font size = 2.5>", name_english, "</font></b></u><br>",
+                    "<b><font color = ", predicted_color, ">", predicted_winner, "</font> hold</b><br>",
+                    "<br>",
+                    "<b><u>Win probabilities</u></b><br>",
+                    "<font color = 'red'><b>", LPC_cand, "</b> (LPC)</font>: ", round(100*LPC_prob), "%<br>",
+                    "<font color = 'blue'><b>", CPC_cand, "</b> (CPC)</font>: ", round(100*CPC_prob), "%<br>",
+                    "<font color = '#EE7600'><b>", NDP_cand, "</b> (NDP)</font>: ", round(100*NDP_prob), "%<br>",
+                    "<font color = '#8B008B'><b>", Bloc_cand, "</b> (Bloc)</font>: ", round(100*Bloc_prob), "%<br>",
+                    "<font color = '#008B00'><b>", Green_cand, "</b> (Green)</font>: ", round(100*Green_prob), "%<br>",
+                    "<font color = '#191970'><b>", PPC_cand, "</b> (PPC)</font>: ", round(100*PPC_prob), "%<br>",
+                    "<br>",
+                    "<b><u>Projected vote (90% CI)</u></b><br>",
+                    "<font color = 'red'><b>LPC</b></font>: ", round(100*pct_50.LPC, 1), "% (", round(100*pct_05.LPC, 1), "%–", round(100*pct_95.LPC, 1), "%)<br>",
+                    "<font color = 'blue'><b>CPC</b></font>: ", round(100*pct_50.CPC, 1), "% (", round(100*pct_05.CPC, 1), "%–", round(100*pct_95.CPC, 1), "%)<br>",
+                    "<font color = '#EE7600'><b>NDP</b></font>: ", round(100*pct_50.NDP, 1), "% (", round(100*pct_05.NDP, 1), "%–", round(100*pct_95.NDP, 1), "%)<br>",
+                    "<font color = '#8B008B'><b>Bloc</b></font>: ", round(100*pct_50.Bloc, 1), "% (", round(100*pct_05.Bloc, 1), "%–", round(100*pct_95.Bloc, 1), "%)<br>",
+                    "<font color = '#008B00'><b>Green</b></font>: ", round(100*pct_50.Green, 1), "% (", round(100*pct_05.Green, 1), "%–", round(100*pct_95.Green, 1), "%)<br>",
+                    "<font color = '#191970'><b>PPC</b></font>: ", round(100*pct_50.PPC, 1), "% (", round(100*pct_05.PPC, 1), "%–", round(100*pct_95.PPC, 1), "%)"
+                    ),
+           
+           name_english == "Beauce" & predicted_winner != last_winner ~ 
+             paste0("<b><u><font size = 2.5>", name_english, "</font></b></u><br>",
+                    "<b><font color = ", predicted_color, ">", predicted_winner, "</font> gain from ", "<font color = ", last_color, ">", last_winner, "</font></b><br>",
+                    "<br>",
+                    "<b><u>Win probabilities</u></b><br>",
+                    "<font color = 'red'><b>", LPC_cand, "</b> (LPC)</font>: ", round(100*LPC_prob), "%<br>",
+                    "<font color = 'blue'><b>", CPC_cand, "</b> (CPC)</font>: ", round(100*CPC_prob), "%<br>",
+                    "<font color = '#EE7600'><b>", NDP_cand, "</b> (NDP)</font>: ", round(100*NDP_prob), "%<br>",
+                    "<font color = '#8B008B'><b>", Bloc_cand, "</b> (Bloc)</font>: ", round(100*Bloc_prob), "%<br>",
+                    "<font color = '#008B00'><b>", Green_cand, "</b> (Green)</font>: ", round(100*Green_prob), "%<br>",
+                    "<font color = '#191970'><b>", PPC_cand, "</b> (PPC)</font>: ", round(100*PPC_prob), "%<br>",
+                    "<br>",
+                    "<b><u>Projected vote (90% CI)</u></b><br>",
+                    "<font color = 'red'><b>LPC</b></font>: ", round(100*pct_50.LPC, 1), "% (", round(100*pct_05.LPC, 1), "%–", round(100*pct_95.LPC, 1), "%)<br>",
+                    "<font color = 'blue'><b>CPC</b></font>: ", round(100*pct_50.CPC, 1), "% (", round(100*pct_05.CPC, 1), "%–", round(100*pct_95.CPC, 1), "%)<br>",
+                    "<font color = '#EE7600'><b>NDP</b></font>: ", round(100*pct_50.NDP, 1), "% (", round(100*pct_05.NDP, 1), "%–", round(100*pct_95.NDP, 1), "%)<br>",
+                    "<font color = '#8B008B'><b>Bloc</b></font>: ", round(100*pct_50.Bloc, 1), "% (", round(100*pct_05.Bloc, 1), "%–", round(100*pct_95.Bloc, 1), "%)<br>",
+                    "<font color = '#008B00'><b>Green</b></font>: ", round(100*pct_50.Green, 1), "% (", round(100*pct_05.Green, 1), "%–", round(100*pct_95.Green, 1), "%)<br>",
+                    "<font color = '#191970'><b>PPC</b></font>: ", round(100*pct_50.PPC, 1), "% (", round(100*pct_05.PPC, 1), "%–", round(100*pct_95.PPC, 1), "%)"
+             ),
            
            ## Holds outside Quebec
            PROVCODE != "QC" & predicted_winner == last_winner ~
@@ -129,8 +177,9 @@ canada_flips <- canada_districts_latlong %>%
 ## Leaflet map
 leaflet() %>%
   addPolygons(data = canada_districts_latlong, weight = 1, color = "#666666", opacity = 1, fillColor = ~fill_color, 
-              fillOpacity = ~(pmax(2*max_prob - 1, 0.01))^0.5, label = ~name_english, popup = ~district_info,
+              fillOpacity = ~((pmax(2*max_prob - 1, 0.01))^0.5)/1.1, label = ~name_english, popup = ~district_info,
               highlightOptions = highlightOptions(weight = 3, color = "black", bringToFront = TRUE)) %>%
   addPolylines(data = canada_flips, weight = 2, color = "black", opacity = 1, fillOpacity = 0, dashArray = "3") %>%
+  addTiles(options = tileOptions(opacity = 0.4, fillOpacity = 0.75)) %>%
   setView(lng = -96.5, lat = 55, zoom = 4)
   
--- Shiny-app/app.R
+++ Shiny-app/app.R
@@ -7,8 +7,30 @@ library(leaflet)
 
 ui <- fluidPage(
   titlePanel(paste0("Forecast as of ", month(today(), label = TRUE, abbr = FALSE), " ", day(today()), ", ", year(today()))),
-  sidebarPanel(strong("Current forecast:"), width = 2),
-  leafletOutput("forecastmap", height = 800, width = "80%")
+  sidebarLayout(
+    mainPanel = mainPanel(leafletOutput("forecastmap", height = 800, width = "100%")),
+    ## Sidebar
+    sidebarPanel(tags$h3("Current forecast"),
+                 tags$h4("Outcome probabilities"),
+                 HTML(paste0("<table>",
+                              "<tr>",
+                                "<th><u>Party</u></th><th><u>Majority</u></th><th><u>Minority</u></th>",
+                              "</tr>",
+                              "<tr>",
+                                "<td><b><font color = 'blue'>Conservative</font></b></td>", 
+                                "<td>", round(100*outcome_probs[1,2]), "%</td>",
+                                "<td>", round(100*outcome_probs[1,3]), "%</td>",
+                              "</tr>",
+                              "<tr>",
+                                "<td><b><font color = 'red'>Liberal</font></b></td>", 
+                                "<td>", round(100*outcome_probs[2,2]), "%</td>",
+                                "<td>", round(100*outcome_probs[2,3]), "%</td>",
+                              "</tr>",
+                             "</table>")
+                    ),
+                 width = 3),
+    position = "right"
+    )
 )
 
 # Define server logic required to draw a histogram
