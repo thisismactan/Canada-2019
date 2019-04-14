b4aa1d0b6ac6581cca22d5162cb792f046d07ce7
Mac Tan <mactan@gmail.com>
2019-04-13 23:23:16 -0400

    Finished the important parts of the simulator

--- Code/simulate_simple.R
+++ Code/simulate_simple.R
@@ -4,6 +4,23 @@ source("Code/Modeling/model_error_variance.R")
 source("Code/poll_average.R")
 source("Code/shape_2019_data.R")
 
+## Clean district keys again for good measure
+district_key_2013 <- readr::read_csv("Data/Raw/electoral_districts_key_2013.csv", locale = locale(encoding = "LATIN1"))
+district_key_2013 <- district_key_2013 %>%
+  mutate(name_english = gsub("[[:digit:]]|[[:space:]][[:digit:]]", "", name_english),
+         name_english = gsub("--", "–", name_english),
+         name_english = gsub("â€“", "–", name_english), 
+         name_english = gsub("--", "–", name_english),
+         name_english = gsub("Ã©", "é", name_english),
+         name_english = gsub("Ã¨", "è", name_english),
+         name_english = gsub("ÃŽ", "Î", name_english),
+         name_english = gsub("Ã‰", "É", name_english),
+         name_english = gsub("Ã¢", "â", name_english),
+         name_english = gsub("Ã´", "ô", name_english),
+         name_english = gsub("Ã\\\u008e", "Î", name_english),
+         name_english = gsub("Ã\\\u0089", "É", name_english),
+         name_english = gsub("Ã®", "î", name_english))
+
 ## Simulate draws from national and regional polling
 num.iter <- 10000
 
@@ -139,89 +156,116 @@ pct_95.NDP <- compute_district_stats(NDP_district_simulations, FUN = quantile, p
 pct_95.Bloc <- compute_district_stats(Bloc_district_simulations, FUN = quantile, prob = 0.95)
 pct_95.Green <- compute_district_stats(Green_district_simulations, FUN = quantile, prob = 0.95)
 
-#### Vote distributions ####
+## Vote distributions
+LPC_distribution <- tibble(district_code = data_2019.simple$district_code,
+                           name_english = district_key_2013$name_english,
+                           pct_05.LPC,
+                           pct_25.LPC,
+                           pct_50.LPC,
+                           pct_75.LPC,
+                           pct_95.LPC)
 
-## Liberals
-LPC_distribution <- 
+CPC_distribution <- tibble(district_code = data_2019.simple$district_code,
+                           name_english = district_key_2013$name_english,
+                           pct_05.CPC,
+                           pct_25.CPC,
+                           pct_50.CPC,
+                           pct_75.CPC,
+                           pct_95.CPC)
 
-## 
-LPC_preds <- predict(model_LPC.linear, newdata = data_2019.simple)
-CPC_preds <- predict(model_CPC.linear, newdata = data_2019.simple)
-NDP_preds <- predict(model_NDP.linear, newdata = data_2019.simple)
-Bloc_preds <- predict(model_Bloc.linear, newdata = data_2019.simple)
-Green_preds <- predict(model_Green.linear, newdata = data_2019.simple)
+NDP_distribution <- tibble(district_code = data_2019.simple$district_code,
+                           name_english = district_key_2013$name_english,
+                           pct_05.NDP,
+                           pct_25.NDP,
+                           pct_50.NDP,
+                           pct_75.NDP,
+                           pct_95.NDP)
 
-predictions <- tibble(district_code = data_2019.simple$district_code,
-                      name_english = data_2019.simple$name_english,
-                      region = data_2019.simple$region,
-                      population = data_2019.simple$population,
-                      LPC_cand = data_2019.simple$LPC_cand,
-                      CPC_cand = data_2019.simple$CPC_cand,
-                      NDP_cand = data_2019.simple$NDP_cand,
-                      Bloc_cand = data_2019.simple$Bloc_cand,
-                      Green_cand = data_2019.simple$Green_cand,
-                      LPC_lag = data_2019.simple$LPC_lag,
-                      CPC_lag = data_2019.simple$CPC_lag,
-                      NDP_lag = data_2019.simple$NDP_lag,
-                      Bloc_lag = data_2019.simple$Bloc_lag,
-                      Green_lag = data_2019.simple$Green_lag,
-                      LPC_vote = LPC_preds,
-                      CPC_vote = CPC_preds,
-                      NDP_vote = NDP_preds,
-                      Bloc_vote = Bloc_preds*(district_code >= 24000 & district_code <= 24999),
-                      Green_vote = Green_preds) %>%
-  mutate(total_vote = LPC_vote + CPC_vote + NDP_vote + Bloc_vote + Green_vote) %>%
-  mutate(winner = case_when(LPC_vote == pmax(LPC_vote, CPC_vote, NDP_vote, Bloc_vote, Green_vote) ~ "Liberal",
-                            CPC_vote == pmax(LPC_vote, CPC_vote, NDP_vote, Bloc_vote, Green_vote) ~ "Conservative",
-                            NDP_vote == pmax(LPC_vote, CPC_vote, NDP_vote, Bloc_vote, Green_vote) ~ "NDP",
-                            Bloc_vote == pmax(LPC_vote, CPC_vote, NDP_vote, Bloc_vote, Green_vote) ~ "Bloc",
-                            Green_vote == pmax(LPC_vote, CPC_vote, NDP_vote, Bloc_vote, Green_vote) ~ "Green"),
-         winner_lag = case_when(LPC_lag == pmax(LPC_lag, CPC_lag, NDP_lag, Bloc_lag, Green_lag) ~ "Liberal",
-                                CPC_lag == pmax(LPC_lag, CPC_lag, NDP_lag, Bloc_lag, Green_lag) ~ "Conservative",
-                                NDP_lag == pmax(LPC_lag, CPC_lag, NDP_lag, Bloc_lag, Green_lag) ~ "NDP",
-                                Bloc_lag == pmax(LPC_lag, CPC_lag, NDP_lag, Bloc_lag, Green_lag) ~ "Bloc",
-                                Green_lag == pmax(LPC_lag, CPC_lag, NDP_lag, Bloc_lag, Green_lag) ~ "Green"))
+Bloc_distribution <- tibble(district_code = data_2019.simple$district_code,
+                            name_english = district_key_2013$name_english,
+                            pct_05.Bloc,
+                            pct_25.Bloc,
+                            pct_50.Bloc,
+                            pct_75.Bloc,
+                            pct_95.Bloc) %>%
+  mutate(pct_05.Bloc = pct_05.Bloc*(1 - (floor(district_code/1000) != 24)),
+         pct_25.Bloc = pct_25.Bloc*(1 - (floor(district_code/1000) != 24)),
+         pct_50.Bloc = pct_50.Bloc*(1 - (floor(district_code/1000) != 24)),
+         pct_75.Bloc = pct_75.Bloc*(1 - (floor(district_code/1000) != 24)),
+         pct_95.Bloc = pct_95.Bloc*(1 - (floor(district_code/1000) != 24)))
 
-predictions %>%
-  group_by(winner) %>%
-  summarise(seats = n())
+Green_distribution <- tibble(district_code = data_2019.simple$district_code,
+                             name_english = district_key_2013$name_english,
+                             pct_05.Green,
+                             pct_25.Green,
+                             pct_50.Green,
+                             pct_75.Green,
+                             pct_95.Green)
 
-predictions %>%
-  group_by(winner, region) %>%
-  summarise(seats = n()) %>%
-  spread(winner, seats)
+## Winners by district by simulation
+LPC_wins <- (LPC_district_simulations > CPC_district_simulations) &
+  (LPC_district_simulations > NDP_district_simulations) &
+  (LPC_district_simulations > Bloc_district_simulations) &
+  (LPC_district_simulations > Green_district_simulations)
 
-predictions %>%
-  filter(region == "The frigid northlands") %>%
-  group_by(winner, winner_lag) %>%
-  summarise(seats = n()) %>%
-  spread(winner, seats)
+CPC_wins <- (CPC_district_simulations > LPC_district_simulations) &
+  (CPC_district_simulations > NDP_district_simulations) &
+  (CPC_district_simulations > Bloc_district_simulations) &
+  (CPC_district_simulations > Green_district_simulations)
 
-predictions %>%
-  mutate(LPC_swing = LPC_vote - LPC_lag,
-         CPC_swing = CPC_vote - CPC_lag,
-         NDP_swing = NDP_vote - NDP_lag,
-         Bloc_swing = Bloc_vote - Bloc_lag,
-         Green_swing = Green_vote - Green_lag) %>%
-  dplyr::select(district_code, LPC_swing, CPC_swing, NDP_swing, Bloc_swing, Green_swing) %>%
-  melt(id.vars = "district_code", variable.name = "party", value.name = "pp") %>%
-  ggplot(aes(x = pp, fill = party)) +
-  facet_wrap(~party, scales = "free_x") +
-  geom_histogram(col = "black") +
-  scale_fill_manual(name = "Party", values = c("red", "blue", "darkorange1", "#8ECEF9", "green4"),
-                    labels = c("Liberal", "Conservative", "NDP", "Bloc Québécois", "Green"))
+NDP_wins <- (NDP_district_simulations > LPC_district_simulations) &
+  (NDP_district_simulations > CPC_district_simulations) &
+  (NDP_district_simulations > Bloc_district_simulations) &
+  (NDP_district_simulations > Green_district_simulations)
 
-predictions %>%
-  summarise(LPC_votes = sum(LPC_vote*population),
-            CPC_votes = sum(CPC_vote*population),
-            NDP_votes = sum(NDP_vote*population),
-            Bloc_votes = sum(Bloc_vote*population),
-            Green_votes = sum(Green_vote*population)) %>%
-  mutate(LPC_pct = LPC_votes/(LPC_votes+CPC_votes+NDP_votes+Bloc_votes+Green_votes),
-         CPC_pct = CPC_votes/(LPC_votes+CPC_votes+NDP_votes+Bloc_votes+Green_votes),
-         NDP_pct = NDP_votes/(LPC_votes+CPC_votes+NDP_votes+Bloc_votes+Green_votes),
-         Bloc_pct = Bloc_votes/(LPC_votes+CPC_votes+NDP_votes+Bloc_votes+Green_votes),
-         Green_pct = Green_votes/(LPC_votes+CPC_votes+NDP_votes+Bloc_votes+Green_votes)) %>%
-  dplyr::select(ends_with("pct"))
+Bloc_wins <- (Bloc_district_simulations > LPC_district_simulations) &
+  (Bloc_district_simulations > CPC_district_simulations) &
+  (Bloc_district_simulations > NDP_district_simulations) &
+  (Bloc_district_simulations > Green_district_simulations)
 
-## Total covariance matrices (one for each province)
+Green_wins <- (Green_district_simulations > LPC_district_simulations) &
+  (Green_district_simulations > CPC_district_simulations) &
+  (Green_district_simulations > NDP_district_simulations) &
+  (Green_district_simulations > Bloc_district_simulations)
+
+## Win probabilities by district
+district_probs <- tibble(district_code = data_2019.simple$district_code,
+                         name_english = district_key_2013$name_english,
+                         LPC_prob = rowMeans(LPC_wins),
+                         CPC_prob = rowMeans(CPC_wins),
+                         NDP_prob = rowMeans(NDP_wins),
+                         Bloc_prob = rowMeans(Bloc_wins),
+                         Green_prob = rowMeans(Green_wins))
+
+## Seat counts by simulation
+LPC_seats <- colSums(LPC_wins)
+CPC_seats <- colSums(CPC_wins)
+NDP_seats <- colSums(NDP_wins)
+Bloc_seats <- colSums(Bloc_wins)
+Green_seats <- colSums(Green_wins)
+
+seat_simulations <- tibble(simulation = 1:num.iter,
+                           LPC = LPC_seats,
+                           CPC = CPC_seats,
+                           NDP = NDP_seats,
+                           Bloc = Bloc_seats,
+                           Green = Green_seats) %>%
+  mutate(most_seats = case_when(LPC == pmax(LPC, CPC, NDP, Bloc, Green) ~ "Liberal",
+                                CPC == pmax(LPC, CPC, NDP, Bloc, Green) ~ "Conservative",
+                                NDP == pmax(LPC, CPC, NDP, Bloc, Green) ~ "NDP",
+                                Bloc == pmax(LPC, CPC, NDP, Bloc, Green) ~ "Bloc",
+                                Green == pmax(LPC, CPC, NDP, Bloc, Green) ~ "Green"),
+         type_of_win = case_when(pmax(LPC, CPC, NDP, Bloc, Green) <= 338/2 ~ "Minority",
+                                 pmax(LPC, CPC, NDP, Bloc, Green) > 338/2 ~ "Majority"))
+
+## Outcome probabilities
+seat_simulations %>%
+  group_by(most_seats, type_of_win) %>%
+  summarise(prob = n()/num.iter) %>%
+  spread(type_of_win, prob)
+
+seat_simulations %>%
+  group_by(most_seats) %>%
+  summarise(prob = n()/num.iter)
+
+## Seat distributions by party
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Canada-2019
-## Last updated: 6 Apr 2019
+## Last updated: 13 Apr 2019
 This is the repository for the Election StatSheet 2019 Canadian election forecast. Hooray!
 
-Current status: estimating model error variances and decomposition, trying out this cool randomForestExplainer package.
+Current status: writing the simulator.
 
 ### Notes to self ###
 - Final model must predict all parties' share of the vote simultaneously allowing for correlations between parties
