3a215a7952411a4e160d2f19c97f61a600a9426e
Mac Tan <mactan@gmail.com>
2019-04-15 01:01:57 -0400

    Redefined incumbency

--- Code/shape_data.R
+++ Code/shape_data.R
@@ -62,6 +62,11 @@ cands_pre2013 <- bind_rows(cands_2004, cands_2006, cands_2008, cands_2011) %>%
   dplyr::select(district_code, year, party, candidate) %>%
   spread(party, candidate)
 
+incumbents_pre2013 <- bind_rows(read_csv("Data/incumbents_2006.csv") %>% mutate(year = 2006),
+                                read_csv("Data/incumbents_2008.csv") %>% mutate(year = 2008),
+                                read_csv("Data/incumbents_2011.csv") %>% mutate(year = 2011)) %>%
+  dplyr::select(-population, -name_english)
+
 cands_2015 <- fread_to_tbl("Data/candidates_2015.csv") %>% 
   mutate(year = 2015) %>%
   filter(party %in% c("Liberal", "Conservative", "NDP", "Green", "Bloc")) %>%
@@ -79,7 +84,7 @@ results_pre2013 <- bind_rows(results_2004, results_2006, results_2008, results_2
   as.tbl() %>%
   group_by(district_code, name_english, year) %>%
   mutate_at(vars(c("Liberal", "Conservative", "NDP", "Green", "Bloc", "total_votes", "registered_voters")), as.numeric) %>%
-  summarise_at(vars(c("Liberal", "Conservative", "NDP", "Green", "Bloc", "total_votes", "registered_voters")), sum, na.rm = TRUE) %>%
+  summarise_at(vars(c("Liberal", "Conservative", "NDP", "Green", "Bloc", "total_votes", "registered_voters")), sum, na.rm = TRUE) %>% 
   left_join(cands_pre2013, by = c("district_code", "year")) %>%
   select(district_code, name_english, year, LPC_cand = Liberal.y, CPC_cand = Conservative.y, NDP_cand = NDP.y, Green_cand = Green.y, Bloc_cand = Bloc.y, 
          registered_voters, total_votes, LPC_votes = Liberal.x, CPC_votes = Conservative.x, NDP_votes = NDP.x, Green_votes = Green.x, 
@@ -100,14 +105,15 @@ results_pre2013 <- bind_rows(results_2004, results_2006, results_2008, results_2
   group_by(district_code) %>%
   mutate_at(vars(c("winner", "LPC_pct", "CPC_pct", "NDP_pct", "Green_pct", "Bloc_pct")),
             funs(last = lag)) %>%
-  ungroup() %>% 
-  mutate(incumbent = case_when(winner_last == LPC_cand ~ "Liberal",
-                               winner_last == CPC_cand ~ "Conservative",
-                               winner_last == NDP_cand ~ "NDP",
-                               winner_last == Green_cand ~ "Green",
-                               winner_last == Bloc_cand ~ "Bloc"),
-         incumbent = case_when(!is.na(incumbent) ~ incumbent,
-                               is.na(incumbent) ~ "None") %>% as.character()) 
+  ungroup() %>%
+  merge(incumbents_pre2013, by = c("district_code", "year"), all.x = TRUE) %>%
+  as.tbl() %>%
+  mutate(incumbent = case_when(last_winner == "Liberal" & incumbent_running ~ "Liberal",
+                               last_winner == "Conservative" & incumbent_running ~ "Conservative",
+                               last_winner == "NDP" & incumbent_running ~ "NDP",
+                               last_winner == "Green" & incumbent_running ~ "Green",
+                               last_winner == "Bloc" & incumbent_running ~ "Bloc",
+                               !incumbent_running ~ "None"))
 
 ## Long format
 results_pre2013_long <- results_pre2013 %>%
